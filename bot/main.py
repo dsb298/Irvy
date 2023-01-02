2c9473432737f8678d420667f55006ec9f9685ad
Devlin <devlinbabcock71@gmail.com>
2023-01-02 12:24:57 -0500

    rm

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,13 @@
 from server import TcpServer
-import time
+import adafruit_mpu6050
+import board
+time.sleep(1)
 
 HOST = '192.168.1.220'
 PORT = 12345
+
+i2c = board.I2C()
+mpu = adafruit_mpu6050.MPU6050(i2c)
 
 def main():
     myServer = TcpServer()
@@ -10,10 +15,13 @@ def main():
 
     msg = b"Hello from server!"
 
-    # run_code = True
-    # while(run_code):
-    myServer.send_data(msg)
-    time.sleep(1)
+    run_code = True
+    while(run_code):
+        print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+        print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+        # myServer.send_data(msg)
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
